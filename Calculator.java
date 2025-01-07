6a77133d4d18c9031087e08dcc99857743f54682
sganeshwani <ganeshwani2003@gmail.com>
2025-01-07 22:09:48 +0530

    Multiplication functionality added

--- Calculator.java
+++ Calculator.java
@@ -6,11 +6,15 @@ return x + y;
 public static int subtract(int x, int y) {
 return x - y;
 }
+public static int multiply(int x, int y) {
+return x * y;
+}
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 int x = sc.nextInt();
 int y = sc.nextInt();
 System.out.println("the sum is: " + add(x, y));
 System.out.println("the difference is: " + subtract(x, y));
+System.out.println("the multiplication is: " + multiply(x, y));
 }
 }
