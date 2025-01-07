704c28b2ccca51da900926eb9415faef15e78fc3
sganeshwani <ganeshwani2003@gmail.com>
2025-01-07 22:05:57 +0530

    Subtraction functionality added

--- Calculator.java
+++ Calculator.java
@@ -3,10 +3,14 @@ public class Calculator {
 public static int add(int x, int y) {
 return x + y;
 }
+public static int subtract(int x, int y) {
+return x - y;
+}
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 int x = sc.nextInt();
 int y = sc.nextInt();
 System.out.println("the sum is: " + add(x, y));
+System.out.println("the difference is: " + subtract(x, y));
 }
 }
