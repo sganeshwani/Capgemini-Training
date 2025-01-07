0a12a0639e8d1107571b1c0ba53b13295537c3ef
sganeshwani <ganeshwani2003@gmail.com>
2025-01-07 22:03:08 +0530

    Addition functionality added

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,12 @@
 import java.util.*;
 public class Calculator {
+public static int add(int x, int y) {
+return x + y;
+}
 public static void main(String[] args) {
-System.out.println("Welcome to Calculator");
+Scanner sc = new Scanner(System.in);
+int x = sc.nextInt();
+int y = sc.nextInt();
+System.out.println("the sum is: " + add(x, y));
 }
 }
